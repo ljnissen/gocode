fc56a6aa6f6b30a5cd8d2a9b4fac96205057b7f1
LJ Nissen <lj.nissen@gmail.com>
2017-07-08 12:59:47 +0200

    Organize FileServer

--- 004/main.go
+++ 004/main.go
@@ -19,7 +19,7 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
-	http.Handle("/assets", http.StripPrefix("/assets", http.FileServer(http.Dir("public/"))))
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("public"))))
 	http.ListenAndServe(":8080", nil)
 }
 
